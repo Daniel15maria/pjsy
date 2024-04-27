f05f5a411af67c16ab35bdf95cc213bd1f5dbb43
triCKY05 <6465913@github>
2024-04-28 02:03:46 +0530

    worldyouthday completed

--- src/components/programs/WorldYouth.js
+++ src/components/programs/WorldYouth.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import youth from '../images/Prog/prog2/img1.png';
+import worldyouth from '../images/Prog/prog4/img1.png';
 import { Footer } from '../resuable/footer';
 import '../../index.css';
-import youthside from '../images/Prog/prog2/img2.png'
-import youthright from '../images/Prog/prog2/img3.png'
+import youthdown from '../images/Prog/prog4/img2.png'
 
 
 const YouthMinistry = () => {
@@ -14,27 +13,25 @@ const YouthMinistry = () => {
 
                 <Container className='d-flex justify-content-center align-items-center'>
                     <Row className='mt-5'>
-                        <h1 className='mt-5'>~ YOUTH MINISTRY COMMISION MEETING ~</h1>
+                        <h1 className='mt-5'>~ WORLD YOUTH DAY - 2023 ~</h1>
                     </Row>
                 </Container>
                 <Container className='d-flex justify-content-center align-items-center'>
                     <Row>
-                        <img alt="mission img" src={youth} className="mt-5 mb-3" style={{ maxWidth: '1000px' }} />
+                        <img alt="mission img" src={worldyouth} className="mt-5 mb-3" style={{ maxWidth: '1000px' }} />
                     </Row>
                 </Container>
                 <Container>
                     <Row className='lead pb-5 h-1'>
-                        <strong><h3> We had the youth commission meeting from 10-12 March 2023 in Vidya Niwas, Varanasi. It was a very fruitful meeting as we have planned meticulously for the forthcoming year. We evaluated our works of the past year and discussed about the coming events of the year. We have organized “Summer Skill Development Program 2023” from 23 April to 8 May 2023 at Atmadarshan, Patna. There are total of 48 inter completed youth; 32 girls and 16 boys from around 17 parishes. They are taught spoken English, writing skills, GANGA LAHAR – MAY - JUNE, 2023 12 Biology, speech drills, certificate computer course, motivation, Spirituality and so on. Fr. Joseph Sebastian, Fr. Martin and the college community have been very helpful to us in this program. Our sincere thanks to all those who helped us in this program, very specially Sr. Monica SCN, Sr. Priya SCN, Frs. Christy, Sushil, Pulikal, Jeevan, Sebastian Alphonse, Sijo, Sr.Anita CJ, Sr. Pushpa, Mr. Amarjeet, Fr. Alwyn and the team.</h3></strong>
+                        <strong><h3> We had MAGIS LISBON 2023 and World Youth Day in July and August 2023 in Portugal. We were 41 members from JCSA delegation (35 Youth and 6 Jesuits from different Zones). From Our Province, Mr. Aryan Samuel from Ara, Miss. Anisha Anthony from SXCMT and myself went to Portugal. We reached Lisbon on 21 July 2023. On the 23rd we went to Fatima and spent the day there. We had two days program in Lisbon and then we were sent to different places for our Ignatian experiments. I went to Spain for the pilgrimage (walking to Santiago de Compostella). Our two youth were in Portugal for the experiment. Again, we came to Lisbon on 29 July night. On the 31st Fr. General celebrated the Feast Day Eucharistic celebration. On that day evening we ended the MAGIS 2023. Then we began the ‘World Youth Day’ from 1 to 6 August. It was very enriching to see the sea of youth and their faith. We all had wonderful time and experiences.
+                            Then we had AICUF national council meeting from 12 to 15 August 2023 in Chennai. Fr. Prakash went to Chennai from Mumbai. Fr. Jeevan took 5 youth from Patna and Varanasi and reached Chennai on the 12th morning. Our youth participated in the meeting enthusiastically. Then Fr. Prakash went to Vijayawada to participate in JYMSA annual meeting. - Prakash Antony
+                        </h3></strong>
                     </Row>
                 </Container>
                 <div>
                     <Row>
-                        <Col lg={6}>
-                            <img src={youthside} alt='youth side pic' className="img-fluid my-5" />
-                        </Col>
-                        <Col lg={6}>
-                            <img src={youthright} alt='youth side pic' className="img-fluid my-5" />
-                        </Col>
+
+                        <img src={youthdown} alt='youthdownpic' className="img-fluid my-5" />
                     </Row>
                 </div>
             </div>
