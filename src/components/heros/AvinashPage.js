33519aa3bb65268a226edc56e586dd82072836a6
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-04-28 11:22:43 +0530

    avinash pages

--- src/components/heros/AvinashPage.js
+++ src/components/heros/AvinashPage.js
@@ -1,13 +1,13 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import herosbg from '../images/herosimg.png';
 import { Footer } from '../resuable/footer';
-import avinash1 from '../images/Heros/avinash/avinash1.jpg';
+import avinash1 from '../images/Heros/avinash/avinash7.jpg';
 import avinash2 from '../images/Heros/avinash/avinash2.jpg';
 import avinash3 from '../images/Heros/avinash/avinash3.jpg';
 import avinashtop from '../images/Heros/avinash/avinash5.jpg';
 import avinash5 from '../images/Heros/avinash/avinash9.jpg';
 import avinash6 from '../images/Heros/avinash/avinash6.jpg';
-import avinash7 from '../images/Heros/avinash/avinash7.jpg';
+import avinash7 from '../images/Heros/avinash/avinash1c.jpg';
 import '../../index.css';
 
 const AvinashPage = () => {
@@ -19,14 +19,18 @@ const AvinashPage = () => {
                     <div className='each-head text-center my-3' >
                         Rising from Ashes: The AVINASH MAHATO Story
                     </div>
-                    <Col lg={3} className='p-1 d-flex align-items-center justify-content-center'>
-                        <img src={avinashtop} alt="avinash img 11" className="img-fluid" />
+                    <Col lg={4}></Col>
+                    <Col lg={8}><h2>“Avinash’s existence was etched in shadows” </h2></Col>
+
+
+                    <Col lg={4} className='p-2 d-flex align-items-center justify-content-center'>
+                        <img src={avinashtop} alt="avinash img" className="img-fluid" />
                     </Col>
-                    <Col lg={1}></Col>
+                    {/* <Col lg={1}></Col> */}
                     <Col lg={8}>
                         <div className='lead py-4'>
-                            <h2>“Avinash’s existence was etched in shadows” </h2>
-                            <br />
+
+
                             <strong> In the heart of a forgotten village, where poverty clung to every mud-brick wall, Avinash Mahato’s life unfolded like a tattered scroll. His father, a drunken specter, had sold their ancestral land, leaving the family adrift in a sea of deprivation. His mother, a silent warrior, stitched together their survival with frayed threads of love and resilience.
                                 Avinash’s existence was etched in shadows - the forgotten child of a forsaken corner house. The Tharu community, belittled and excommunicated, whispered tales of their misfortune. Yet, within this desolation, a spark flickered - a hunger for knowledge, a thirst for learning and a desire for escape.There come the Jesuits of Patna Province - their presence like a celestial alignment. Three JESUITS of purpose, their cassocks billowing with hope. They saw Avinash’s potential, hidden beneath rags and hunger. Their plan was audacious: to nurture a boy who wasn’t their kin, who had been robbed of childhood, who clung to the fringes of relevance. The process began - a symphony of mentorship and education. Avinash, studious and ambitious, absorbed knowledge like parched earth after rain. His energy, once scattered, found rhythm - a channeling of purpose. Computers hummed under his fingertips, and Tally sheets danced to his command.</strong>
 
@@ -39,15 +43,15 @@ const AvinashPage = () => {
                     <Col lg={6} className='lead py-4 d-flex align-items-center justify-content-center'>
                         <div>
                             <strong> But the agreement weighed heavy. He would halt his own studies, dedicating a year to service. The center became his sanctuary - a place where bytes and balance sheets intertwined. Amidst the whir of machines, he discovered more than algorithms; he found himself. Transitional moments arrived. Avinash faced his father - a man who had abandoned love for liquor. Their conversations were bridges - rickety but vital. He accepted his mentally handicapped brother, bridging chasms of disassociation. Healing flowed like monsoon rain, washing away anger and hurt.
-                                Challenges loomed. Poverty gnawed at his dreams. The internal battle—the need to prove himself, to defy failure’s icy grip—threatened to consume him. Yet, he persisted. Fluency in English became his armor, confidence its sheen. Avinash was transformed—an emotional tempest now a steady ship navigating life’s tempests.</strong>
+                                Challenges loomed. Poverty gnawed at his dreams. The internal battle—the need to prove himself, to defy failure’s icy grip—threatened to consume him. </strong>
 
                         </div>
                     </Col>
                     <Col lg={3} className='d-flex align-items-center justify-content-center'>
-                        <img src={avinash1} alt="avinash img 1" className="img-fluid" style={{ height: '300px' }} />
+                        <img src={avinash1} alt="avinash img 1" className="img-fluid" style={{ height: '320px' }} />
                     </Col>
                     <Col lg={3} className='d-flex align-items-center justify-content-center'>
-                        <img src={avinash2} alt="avinash img 2" className="img-fluid" style={{ height: '300px' }} />
+                        <img src={avinash2} alt="avinash img 2" className="img-fluid" style={{ height: '320px' }} />
                     </Col>
                 </Row>
             </Container>
@@ -59,14 +63,14 @@ const AvinashPage = () => {
 
                     <Col lg={8}>
                         <div className='lead py-5 px-2'>
-                            <strong>  And so, he pursued M.Com - a beacon of resilience. The boy who once clenched fists in rage now faced life’s storms with courage. His brand story? It echoed through the village - a testament to the Society of Jesus, to mentors who believed in more than textbooks. Avinash Mahato, the phoenix rising from the ashes - carved his legacy. His footsteps, once hesitant, now imprinted the path for others. Poverty no longer defined him; it fueled his ascent. And as he balanced ledgers and dreams, he whispered, “I am enough.”</strong>
+                            <strong> Yet, he persisted. Fluency in English became his armor, confidence its sheen. Avinash was transformed—an emotional tempest now a steady ship navigating life’s tempests. And so, he pursued M.Com - a beacon of resilience. The boy who once clenched fists in rage now faced life’s storms with courage. His brand story? It echoed through the village - a testament to the Society of Jesus, to mentors who believed in more than textbooks. Avinash Mahato, the phoenix rising from the ashes - carved his legacy. His footsteps, once hesitant, now imprinted the path for others. Poverty no longer defined him; it fueled his ascent. And as he balanced ledgers and dreams, he whispered, “I am enough.”</strong>
                         </div>
                     </Col>
                 </Row>
             </Container>
             <h4 className='text-center'>The Motivating Moments Of Avinash Mahato</h4>
             <Container>
-                <Row>
+                <Row style={{ height: '680px', overflow: 'hidden' }}>
                     <Col lg={4}><img src={avinash3} alt="avinash img 5" className="img-fluid" />
                     </Col>
                     <Col lg={4}><img src={avinash6} alt="avinash img 6" className="img-fluid" />
--- src/components/programs/YouthMinistry.js
+++ src/components/programs/YouthMinistry.js
@@ -19,24 +19,24 @@ const YouthMinistry = () => {
                 </Container>
                 <Container className='d-flex justify-content-center align-items-center'>
                     <Row>
-                        <img alt="mission img" src={youth} className="mt-5 mb-3" style={{ maxWidth: '1000px' }} />
+                        <img alt="mission img" src={youth} className="mt-4 mb-3" style={{ maxWidth: '1000px' }} />
                     </Row>
                 </Container>
                 <Container>
-                    <Row className='lead pb-5 h-1'>
+                    <Row className='lead pb-2 mt-4 h-1'>
                         <strong><h3> We had the youth commission meeting from 10th-12th March 2023 in Vidya Niwas, Varanasi.
-                             It was a very fruitful meeting as we have planned meticulously for the forthcoming year. We evaluated 
-                             our works of the past year and discussed about the coming events of the year. We have organized “Summer 
-                             Skill Development Program 2023” from 23 April to 8 May 2023 at Atmadarshan, Patna. There are total of 48 
-                             inter completed youth; 32 girls and 16 boys from around 17 parishes. They are taught spoken English, writing
-                              skills, GANGA LAHAR – MAY - JUNE, 2023 12 Biology, speech drills, certificate computer course, motivation, 
-                              Spirituality and so on. Fr. Joseph Sebastian, Fr. Martin and the college community have been very helpful to 
-                              us in this program. Our sincere thanks to all those who helped us in this program, very specially Sr. Monica 
-                              SCN, Sr. Priya SCN, Frs. Christy, Sushil, Pulikal, Jeevan, Sebastian Alphonse, Sijo, Sr.Anita CJ, Sr. Pushpa, 
-                              Mr. Amarjeet, Fr. Alwyn and the team.</h3></strong>
+                            It was a very fruitful meeting as we have planned meticulously for the forthcoming year. We evaluated
+                            our works of the past year and discussed about the coming events of the year. We have organized “Summer
+                            Skill Development Program 2023” from 23 April to 8 May 2023 at Atmadarshan, Patna. There are total of 48
+                            inter completed youth; 32 girls and 16 boys from around 17 parishes. They are taught spoken English, writing
+                            skills, GANGA LAHAR – MAY - JUNE, 2023 12 Biology, speech drills, certificate computer course, motivation,
+                            Spirituality and so on. Fr. Joseph Sebastian, Fr. Martin and the college community have been very helpful to
+                            us in this program. Our sincere thanks to all those who helped us in this program, very specially Sr. Monica
+                            SCN, Sr. Priya SCN, Frs. Christy, Sushil, Pulikal, Jeevan, Sebastian Alphonse, Sijo, Sr.Anita CJ, Sr. Pushpa,
+                            Mr. Amarjeet, Fr. Alwyn and the team.</h3></strong>
                     </Row>
                 </Container>
-                <div>
+                <Container>
                     <Row>
                         <Col lg={6}>
                             <img src={youthside} alt='youth side pic' className="img-fluid my-5" />
@@ -45,7 +45,7 @@ const YouthMinistry = () => {
                             <img src={youthright} alt='youth side pic' className="img-fluid my-5" />
                         </Col>
                     </Row>
-                </div>
+                </Container>
             </div>
 
             <Footer />
